b63cf091d14fcb9e22cfc5aee2746fcaef42c505
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-20 16:19:49 -0300

    chore: routes settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import Routes from './routes';
 
 export default function App() {
-  return <Text>GoBarber APP</Text>;
+  return <Routes />;
 }
